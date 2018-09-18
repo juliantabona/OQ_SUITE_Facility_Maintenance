d5a6ddb20f76ce148f540263dad7e8a3a6927370
Julian <brandontabona@gmail.com>
2018-09-18 11:29:10 +0200

    update

--- config/mail.php
+++ config/mail.php
@@ -55,8 +55,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@internationaldataweek.org'),
+        'name' => env('MAIL_FROM_NAME', 'International Data Week 2018'),
     ],
 
     /*
