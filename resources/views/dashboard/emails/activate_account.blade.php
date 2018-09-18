1963e00098f8710c00ec5ac7d11d3ce619a4a4a3
Julian <brandontabona@gmail.com>
2018-09-18 11:50:19 +0200

    update

--- resources/views/dashboard/emails/activate_account.blade.php
+++ resources/views/dashboard/emails/activate_account.blade.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Facility Maintenance</title>
+<title>{{ env('APP_NAME', 'OQ - Facility Maintenance') }}</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
 </head>
-<body>
+<body style="margin: 0 !important; padding: 0 !important;">
         Hello Tabona, thank you for taking your time to be a part of our great platform. Go ahead and activate your account and lets get started!
 </body>
 </html>
