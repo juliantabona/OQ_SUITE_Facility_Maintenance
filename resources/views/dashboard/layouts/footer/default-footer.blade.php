17e6f455c6727036ed7dfb6b429a5a36ca256796
Julian <brandontabona@gmail.com>
2018-09-18 10:06:56 +0200

    update

--- resources/views/dashboard/layouts/footer/default-footer.blade.php
+++ resources/views/dashboard/layouts/footer/default-footer.blade.php
@@ -1,6 +1,7 @@
 <footer class="footer">
     <div class="container-fluid clearfix">
         <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © {{ date("Y") }}
-            <a href="#">Optimum Q</a>. All rights reserved.</span>
+        <a href="/">Optimum Q</a>. All rights reserved.</span>
+        <img src="/images/assets/logo/OQ-INFINITE-B-150X84.gif" alt="logo" class="d-block m-auto" style="width: 100px;">
     </div>
 </footer>
--- resources/views/web/layouts/footer/default.php
+++ resources/views/web/layouts/footer/default.php
@@ -3,7 +3,7 @@
         <div class="container">
         <div class="col-lg-4 mx-auto">
             <span class="d-block text-center">Powered By</span> 
-            <img src="http://www.optimumqbw.com/optimumqbw.com/wp-content/uploads/2018/09/OQ-INFINITE-B-150X84.gif" class="d-block m-auto" style="width: 100px;"></div>
+            <img src="/images/assets/logo/OQ-INFINITE-B-150X84.gif" alt="logo" class="d-block m-auto" style="width: 100px;"></div>
         </div>
     </nav>
 </footer>
